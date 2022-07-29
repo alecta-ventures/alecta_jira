e17d0e3fab6e4e4fae2789feefac14bebda41298
notherealbitmos <106530788+notherealbitmos@users.noreply.github.com>
2022-07-29 16:11:31 +0530

    Update DeskopView.js

--- client-app/src/components/DeskopView.js
+++ client-app/src/components/DeskopView.js
@@ -78,7 +78,7 @@ const TodoItem = ({
         );
 
   const filteredCat =
-    query === ""
+    querycat === ""
       ? cat
       : cat.filter((person) =>
           cat.name
@@ -157,7 +157,7 @@ const TodoItem = ({
         <Modal isOpen={editIsShowing}>
           <form
             onSubmit={(e) => handleChanges(e)}
-            className="mt-16 mx-auto max-w-2xl"
+            className="mt-16 relative mx-auto max-w-2xl"
           >
             <div>
               <label
@@ -185,7 +185,7 @@ const TodoItem = ({
                       <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
                         <Combobox.Input
                           className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                          displayValue={(cat) => cat.name}
+                          displayValue={(person) => person.name}
                           onChange={(event) => setQuerycat(event.target.value)}
                         />
                         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
@@ -231,7 +231,7 @@ const TodoItem = ({
                                     </span>
                                     {selected ? (
                                       <span
-                                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                        className={`relative inset-y-0 left-0 flex items-center pl-3 ${
                                           active
                                             ? "text-white"
                                             : "text-teal-600"
@@ -280,7 +280,7 @@ const TodoItem = ({
                         leaveTo="opacity-0"
                         afterLeave={() => setQuery("")}
                       >
-                        <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        <Combobox.Options className="relative mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                           {filteredCat.length === 0 && query !== "" ? (
                             <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                               Nothing found.
@@ -333,7 +333,7 @@ const TodoItem = ({
                 </div>
               </div>
             </div>
-            <div className="mt-8">
+            <div className="mt-8 relative">
               <label
                 for="description"
                 class="block text-sm font-medium leading-5 text-gray-700"
